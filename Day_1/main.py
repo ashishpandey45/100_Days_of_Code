5c7dcf060ecd3a581b7ed804f550185acf061268
Ashish <ashishpandey8840@gmail.com>
2023-05-18 22:18:34 +0530

    Initial commit

---
+++ Day_1/main.py
@@ -0,0 +1,7 @@
+print("Welcome to a Band Name Generator!")
+
+city = input("\nWhat is the name of the city you grew up in?\n")
+
+pet = input("\nWhat is the name of your pet?\n")
+
+print("\nYour band name could be " + city + " " + pet)
---
+++ Day_1/readme.md
@@ -0,0 +1,3 @@
+Band Name Generator
+
+Input the name of your city and the name of your pet and the program will suggest you a name for your band based on the inputs you provided (City name + Pet Name)
